91a073b8b467c3ece1f30a168cab950a0bbdeae1
David Martínez Tapia <dmmtapia.ux@gmail.com>
2023-10-17 12:30:08 -0300

    Update DarkMode.js

--- src/utils/darkMode.js
+++ src/utils/darkMode.js
@@ -11,11 +11,3 @@ if (
     localStorage.theme = "light";
 }
 
-// Whenever the user explicitly chooses light mode
-localStorage.theme = "light";
-
-// Whenever the user explicitly chooses dark mode
-localStorage.theme = "dark";
-
-// Whenever the user explicitly chooses to respect the OS preference
-localStorage.removeItem("theme");
